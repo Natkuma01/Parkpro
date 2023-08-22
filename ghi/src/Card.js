5fc8a5a68989ce1991d0f0b4a7ff7a9f0578f26e
Peter Chiu <pchiu7649@gmail.com>
2023-08-22 19:50:33 +0000

    Card

--- ghi/src/Card.js
+++ ghi/src/Card.js
@@ -1,5 +1,41 @@
-function Card() {
-  return "Card";
-}
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
 
-export default Card;
+
+ export default function ParkCard({ park }) {
+  let randomNumber = Math.floor(Math.random() * (park.images.length - 1))
+  return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          title={park.fullName}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={park.images[randomNumber].url}
+          alt={park.images[randomNumber].title}
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {park.description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+  );
+}
