d170fad693d2ca1319f5fa97dd91e34d603f4470
Sean Cunningham <sc.sean.cunningham@gmail.com>
2023-08-28 18:01:46 -0400

    fix bug in app.js after merge

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -31,6 +31,7 @@ const App = () => {
         console.error(error);
       }
     }
+  });
 
   useEffect(() => {
     fetchParks();
@@ -43,7 +44,10 @@ const App = () => {
         <div className="container">
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/parks" element={<Parks parks={parks} fetchParks={fetchParks} />} />
+            <Route
+              path="/parks"
+              element={<Parks parks={parks} fetchParks={fetchParks} />}
+            />
             <Route path="/wishlist" element={<WishList parks={parks} />} />
             <Route path="/visited" element={<Visited parks={parks} />} />
             <Route path="/login" element={<SignInSide />} />
