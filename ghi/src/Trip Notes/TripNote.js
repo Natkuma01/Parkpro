5ebfb624900f4c7b6c7f573c3c2f5cb657c18f59
Sean Cunningham <sc.sean.cunningham@gmail.com>
2023-09-01 11:33:08 -0400

    combine note form and note display component

--- ghi/src/Trip Notes/TripNote.js
+++ ghi/src/Trip Notes/TripNote.js
@@ -1,13 +1,18 @@
 import TripNoteForm from "./TripNoteForm";
 import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
 import { useEffect, useState } from "react";
+import { Button, Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+
+import { styled } from "@mui/material/styles";
 
 export default function TripNote({ userData, park }) {
-  const parkCode = "yell";
+  const parkCode = park.parkCode;
   const data = JSON.parse(localStorage.getItem("user"));
   const { token } = useAuthContext();
   const [note, setNote] = useState();
   const [tokenLoad, setTokenLoad] = useState(null);
+  const [content, setContent] = useState("");
 
   const getNote = async (parkCode) => {
     const url = `http://localhost:8000/api/trip_note/${parkCode}`;
@@ -31,6 +36,41 @@ export default function TripNote({ userData, park }) {
     }
   };
 
+  const addOrUpdateNote = async (data) => {
+    const createdDate = note.created ? note.created : new Date();
+    const noteInfo = {
+      username: userData["username"],
+      parkCode: park.parkCode,
+      created: createdDate,
+      updated: new Date(),
+      ...data,
+    };
+    const url = `http://localhost:8000/api/note`;
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(note),
+      headers: {
+        Authorization: `Bearer ${tokenLoad}`,
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(url, fetchConfig);
+      if (!response.ok) {
+        console.error("Error updating trip note");
+      } else {
+        console.log("trip note updated");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const ValidationTextField = styled(TextField)({
+    "& .MuiFormLabel-asterisk": {
+      color: "white",
+    },
+  });
+
   useEffect(() => {
     setTokenLoad(token);
   }, []);
@@ -39,12 +79,37 @@ export default function TripNote({ userData, park }) {
     !!tokenLoad && getNote(parkCode);
   }, [tokenLoad]);
 
+  useEffect(() => {
+    const noteContent = note ? note.content : "";
+    setContent(noteContent);
+  }, [note]);
   return (
-    <TripNoteForm
-      userData={data}
-      currentNote={note}
-      park={park}
-      tokenLoad={tokenLoad}
-    />
+    !!note && (
+      <Box
+        component="form"
+        noValidate
+        onSubmit={() => console.log("submit")}
+        sx={{ mt: 1 }}
+      >
+        <ValidationTextField
+          value={content}
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          label="Content"
+          autoComplete="content"
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Add Note
+        </Button>
+      </Box>
+    )
   );
 }
