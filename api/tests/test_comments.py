9f467ab77bb6aee7032f877dbe4d61110ffc4fe0
Sean Cunningham <sc.sean.cunningham@gmail.com>
2023-09-07 20:12:40 -0400

    Pass create_comment unittest

--- api/tests/test_comments.py
+++ api/tests/test_comments.py
@@ -4,19 +4,21 @@ from queries.comment_queries import CommentQueries
 from authenticator import authenticator
 from pprint import pprint
 from pydantic import BaseModel
-from
 
 client = TestClient(app)
 # fake_secret_token = "fake_
-class AccountOut(BaseModel):
-    first_name: str
-    last_name: str
-    username: str
-    email: str
-    id: str
-    visited: list[str]
-    bucket_list: list[str]
-    avatar: dict
+# class AccountIn(BaseModel):
+#     first_name: str
+#     last_name: str
+#     username: str
+#     email: str
+#     id: str
+#     visited: list[str]
+#     bucket_list: list[str]
+#     avatar: dict
+
+async def override_account(account_data: dict):
+    return ({"account_data": account_data})
 
 class MockCommentQueries:
 
@@ -39,23 +41,39 @@ class MockCommentQueries:
 
 def test_create_comment():
     app.dependency_overrides[CommentQueries] = MockCommentQueries
-    app.dependency_overrides[authenticator.get_current_account_data] = AccountOut
+    app.dependency_overrides[authenticator.get_current_account_data] = override_account
     MockCommentQueries.called = True
     json = {
-
+        "id": "string",
+        "title": "string",
+        "content": "string",
+        "posted": "2023-09-06T19:23:22.352000+00:00",
+        "parkCode": "string",
+        "username": "string",
+        "parent_id": "string"
+    }
+    account_data = {
+        "first_name": "string",
+        "last_name": "string",
+        "username": "string",
+        "email": "string",
+        "id": "string",
+        "visited": [],
+        "bucket_list": [],
+        "avatar": {},
     }
     expected = {
-            "id": "string",
-            "title": "string",
-            "content": "string",
-            "posted": "2023-09-06T19:23:22.352Z",
-            "parkCode": "string",
-            "username": "string",
-            "parent_id": "string"
-        }
+        "id": "string",
+        "title": "string",
+        "content": "string",
+        "posted": "2023-09-06T19:23:22.352000+00:00",
+        "parkCode": "string",
+        "username": "string",
+        "parent_id": "string",
+    }
 
-    response = client.post("/api/comments")
-    assert response == expected
+    response = client.post("/api/comments", json=json)
+    assert response.json() == expected
     assert response.status_code == 200
     assert MockCommentQueries.called
     app.dependency_overrides = {}
@@ -78,4 +96,5 @@ def test_get_all_comments():
 
 
     # def update_comment(self, id, comment, account_data):
+
     # def delete_comment(self, id, account_data):
